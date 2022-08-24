8d8fe3db8c96004742535ffe4c5dd2f657993245
Rafael Sá <sanunesrafael@gmail.com>
2022-08-24 13:52:04 -0300

    Add About page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def about
+  end
   #o metodo fica sem codigo por convenção. Ele espera um arquivo .html.erb la em Views/Pages
 end
